73266b4ca308c63b0bb4c72d010e1b2742b674da
frdgrbg <frdgrbg@gmail.com>
2026-04-05 14:49:56 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dpwt-v2';
+const CACHE='dpwt-v3';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{
